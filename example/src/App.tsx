ca5f47530eb5770222fdbe096eaed16a3a1b79a4
rasana <rasana@hivepro.com>
2021-10-11 19:55:20 +0530

    @rassana created react http service using axios

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,5 @@
 import 'react-axios/dist/index.css'
 import React from 'react';
-
 const App = () => {
   return <div>To create library creat-react-library library_name then in src command npm start it will generate dist and to view our changes cd to example then run npm start </div>
 }
